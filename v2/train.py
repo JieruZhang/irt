6db2c8f1cb4de8a011b4ec847f0c02a54f07b95b
张洁茹 <zhangjieru@zhangjierudeMBP.lan>
2019-04-21 22:08:27 +0800

    add new models

---
+++ v2/train.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   train.py.py    
+@Contact :   jrzhanged@163.com
+
+@Modify Time      @Author    @Version    @Desciption
+------------      -------    --------    -----------
+2019-04-21 19:05   jrzhang      1.0         None
+'''
