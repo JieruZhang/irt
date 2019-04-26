7aed6bb874c211785aa4c65246385b7b3cc4f4db
Zhang Jie Ru <zhangjr@fenbi.com>
2019-04-26 11:30:57 +0800

    changes

--- v2/Irm.py
+++ v2/Irm.py
@@ -2,29 +2,32 @@ import numpy as np
 
 
 class Irm(object):
-    def __init__(self, b=0):
+    def __init__(self, a=1, b=0, c=0, model='b'):
         self.b = b
+        self.a = a
+        self.c = c
+        self.model = model
         self.threshold = 1e-5
         self.iters = 50
         self.lr = 0.02
 
     def z(self, theta):
-        return np.exp(self.b - theta)
+        return np.exp(self.a * (self.b - theta))
 
     def p(self, theta):
-        return 1 / (1 + self.z(theta))
+        return (1 / (1 + self.z(theta))) * (1 - self.c) + self.c
 
     def init_b(self, ys):
         """
-        假设所有用户在该知识点上的能力均为0，初始化b
+        假设所有用户在该知识点上的能力均为0，各个题目的区分度均为1， 初始化b
         :param ys:
         :return:
         """
         prob = ys.count(1) / len(ys)
-        if prob == 0 or prob == 1:
+        if prob == self.c or prob == 1:
             return self.b
         else:
-            self.b = np.log(1 / prob - 1)
+            self.b = np.log((1 - self.c) / (prob - self.c) - 1) / self.a
             return self.b
 
     def update_theta(self, user2scores, user2theta):
@@ -34,90 +37,45 @@ class Irm(object):
         :param user2theta: dict, key is userid, value is theta
         :return:
         """
-        # for user in user2theta.keys():
-        #     ys = user2scores[user]
-        #     prob = ys.count(1) / len(ys)
-        #     iteration = 1
-        #     while iteration <= self.iters:
-        #         predict_prob = self.p(user2theta[user])
-        #         #if iteration == self.iters:
-        #             #print("theta, no convergence, max iteration.")
-        #         if np.abs(predict_prob - prob) < self.threshold:
-        #             #print("theta, convergence at iteration: ", iteration)
-        #             break
-        #         z = self.z(user2theta[user])
-        #         gradient = 2 * (predict_prob - prob) * z / ((1 + z) * (1 + z))
-        #         user2theta[user] -= self.lr * gradient
-        #         iteration += 1
-        #####################################
+        ##################内层单轮迭代－平方差损失####################
         for user in user2theta.keys():
             ys = user2scores[user]
             prob = ys.count(1) / len(ys)
-            if prob == 0 or prob == 1:
-                continue
-            else:
-                user2theta[user] = self.b - np.log(1 / prob - 1)
-        ######################################
-        # for user in user2theta.keys():
-        #     ys = user2scores[user]
-        #     prob = ys.count(1) / len(ys)
-        #     predict_prob = self.p(user2theta[user])
-        #     z = self.z(user2theta[user])
-        #     gradient = 2 * (predict_prob - prob) * z / ((1 + z) * (1 + z))
-        #     user2theta[user] -= self.lr * gradient
-
+            predict_prob = self.p(user2theta[user])
+            z = self.z(user2theta[user])
+            gradient = 2 * (predict_prob - prob) * self.a * (1 - self.c) * z / ((1 + z) * (1 + z))
+            user2theta[user] -= self.lr * gradient
         return user2theta
 
-    def update_b(self, user2scores, user2theta):
+    def update_params(self, user2scores, user2theta):
         """
         更新该知识点对应的难度b
         :param user2scores:
         :param user2theta:
         :return:
         """
-        # num_users = len(user2theta.keys())
-        # iteration = 1
-        # while iteration <= self.iters:
-        #     gradient = 0
-        #     error = 0
-        #     for user in user2theta.keys():
-        #         ys = user2scores[user]
-        #         prob = ys.count(1)/len(ys)
-        #         predict_prob = self.p(user2theta[user])
-        #         error += np.abs(predict_prob - prob)
-        #         z = self.z(user2theta[user])
-        #         gradient += (- 2 * (predict_prob - prob) * z / ((1 + z) * (1 + z)))
-        #     error /= num_users
-        #     gradient /= num_users
-        #     #if iteration == self.iters:
-        #         #print("b, no convergence, max iteration.", iteration)
-        #     if error < self.threshold:
-        #         #print("b, convergence at iteration: ", iteration)
-        #         break
-        #     self.b -= self.lr * gradient
-        #     iteration += 1
-        #################################
-        bs = []
+        ##################内层单轮迭代－平方差损失####################
+        num_users = len(user2theta.keys())
+        gradient_b = 0
+        gradient_a = 0
+
         for user in user2theta.keys():
             ys = user2scores[user]
-            prob = ys.count(1) / len(ys)
-            if prob == 0 or prob == 1:
-                bs.append(self.b)
-            else:
-                bs.append(user2theta[user] + np.log(1 / prob - 1))
-        self.b = sum(bs)/len(bs)
-        #############################
-        # num_users = len(user2theta.keys())
-        # gradient = 0
-        # error = 0
-        # for user in user2theta.keys():
-        #     ys = user2scores[user]
-        #     prob = ys.count(1)/len(ys)
-        #     predict_prob = self.p(user2theta[user])
-        #     error += np.abs(predict_prob - prob)
-        #     z = self.z(user2theta[user])
-        #     gradient += (- 2 * (predict_prob - prob) * z / ((1 + z) * (1 + z)))
-        # gradient /= num_users
-        # self.b -= self.lr * gradient
+            prob = ys.count(1)/len(ys)
+            predict_prob = self.p(user2theta[user])
+            z = self.z(user2theta[user])
+            gradient_b += (- 2 * (predict_prob - prob) * self.a * z * (1 - self.c) / ((1 + z) * (1 + z)))
+        gradient_b /= num_users
+        self.b -= self.lr * gradient_b
 
-        return self.b
+        if self.model == 'abc' or self.model == 'ab':
+            for user in user2theta.keys():
+                ys = user2scores[user]
+                prob = ys.count(1) / len(ys)
+                predict_prob = self.p(user2theta[user])
+                z = self.z(user2theta[user])
+                gradient_a += (2 * (predict_prob - prob) * (user2theta[user] - self.b) * z * (1 - self.c) / ((1 + z) * (1 + z)))
+            gradient_a /= num_users
+            self.a -= self.lr * gradient_a
+
+        return self.a, self.b
--- v2/train.py
+++ v2/train.py
@@ -37,7 +37,7 @@ def getData(file_list):
 
 def getSharedKey2Users(dic1, dic2):
     """
-    得到两组数据里面交集的key和user, 筛选掉只有一次做题纪录的case
+    得到两组数据里面交集的key和user
     :param dic1:
     :param dic2:
     :return:
@@ -85,6 +85,7 @@ def getSharedKey2Users(dic1, dic2):
         for key in shared_type2keys2users[skillType].keys():
             shared_users2scores = {}
             for user in shared_type2keys2users[skillType][key]:
+                # 过滤掉做题训练集和测试集上，对于每个知识点，每个用户做题次数少于三次的纪录
                 if len(dic1[skillType][key][user]) < 3 or len(dic2[skillType][key][user]) < 3:
                     continue
                 else:
@@ -97,8 +98,12 @@ def getSharedKey2Users(dic1, dic2):
     return shared_type2keys2users2score
 
 
-
 def getPreparedDataForOneKey(user2scores):
+    '''
+    对于每一个知识点，处理数据
+    :param user2scores:
+    :return:
+    '''
     user2scores_train = {}
     user2scores_test = {}
     user2theta = {}
@@ -113,59 +118,106 @@ def getPreparedDataForOneKey(user2scores):
     return user2scores_train, user2scores_test, user2theta, total_ys_train, total_ys_test
 
 
-def train(user2scores_train, user2scores_test, user2theta, total_ys_train, total_ys_test, iters, tolerance):
-    # train
-    start = time.time()
-    irm = Irm()
-    irm.init_b(total_ys_train)
-    iteration = 1
-    total_error = 0
-    while iteration <= iters:
-        user2theta = irm.update_theta(user2scores_train, user2theta)
-        irm.update_b(user2scores_train, user2theta)
+def train(user2scores_train, user2scores_test, user2theta, total_ys_train, total_ys_test, iters, tolerance, model_names):
+    '''
+    对于每个知识点，训练对应的各模型
+    :param user2scores_train:
+    :param user2scores_test:
+    :param user2theta:
+    :param total_ys_train:
+    :param total_ys_test:
+    :param iters:
+    :param tolerance:
+    :return:
+    '''
+
+    models = []
+    if "b" in model_names:
+        irm_b = Irm(model='b')
+        irm_b.init_b(total_ys_train)
+        models.append(irm_b)
+    if "ab" in model_names:
+        irm_ab = Irm(model='ab')
+        irm_ab.init_b(total_ys_train)
+        models.append(irm_ab)
+    if "bc" in model_names:
+        irm_bc = Irm(model='bc', c=0.1)
+        irm_bc.init_b(total_ys_train)
+        models.append(irm_bc)
+    if "abc" in model_names:
+        irm_abc = Irm(model='abc', c=0.1)
+        irm_abc.init_b(total_ys_train)
+        models.append(irm_abc)
+
+    res_dic = {}
+
+    for irm in models:
+
+        total_error = 0
+        origin_b = irm.b
+        iteration = 1
+
+        # 训练每个模型之前，初始化用户能力
+        for user in user2theta.keys():
+            user2theta[user] = 0
+
+        start = time.time()
+        while iteration <= iters:
+            user2theta = irm.update_theta(user2scores_train, user2theta)
+            irm.update_params(user2scores_train, user2theta)
+            for user in user2scores_train.keys():
+                ys = user2scores_train[user]
+                total_error += np.abs(irm.p(user2theta[user]) - ys.count(1) / len(ys))
+            total_error /= len(user2theta.keys())
+            if iteration == iters:
+                print("No outer convergence.")
+            if total_error < tolerance:
+                print("Outer convergence at outer iteration: ", iteration)
+                break
+            iteration += 1
+        end = time.time()
+        print("Training time for model " + irm.model + " is: ", end - start)
+
+        # 用户训练集上准确率
+        user_old_prob = []
         for user in user2scores_train.keys():
             ys = user2scores_train[user]
-            total_error += np.abs(irm.p(user2theta[user]) - ys.count(1) / len(ys))
-        total_error /= len(user2theta.keys())
-        if iteration == iters:
-            print("No outer convergence.")
-        if total_error < tolerance:
-            print("Outer convergence at outer iteration: ", iteration)
-            break
-        iteration += 1
-    end = time.time()
-    print("Training time: ", end - start)
-    print("Trained b: ", irm.b)
+            user_old_prob.append(ys.count(1) / len(ys))
 
-    # test
-    # 若不更新模型，预测结果
-    irm_origin = Irm()
-    irm_origin.init_b(total_ys_train)
-    print("None trained b: ", irm_origin.b)
-    user_predict_prob_origin = [irm_origin.p(0) for _ in range(len(user2scores_test.keys()))]
+        # 若训练模型，预测结果
+        user_predict_prob = []
+        user_true_prob = []
+        train_test_ratios = []
+        for user in user2scores_test.keys():
+            ys = user2scores_test[user]
+            user_true_prob.append(ys.count(1) / len(ys))
+            user_predict_prob.append(irm.p(user2theta[user]))
+            train_test_ratios.append(len(user2scores_train[user]) / len(user2scores_test[user]))
 
-    # 若训练模型，预测结果
-    user_predict_prob = []
-    user_true_prob = []
-    for user in user2scores_test.keys():
-        ys = user2scores_test[user]
-        user_true_prob.append(ys.count(1) / len(ys))
-        user_predict_prob.append(irm.p(user2theta[user]))
+        acc_diff = sum(np.abs(np.array(user_old_prob) - np.array(user_true_prob))) / len(user_true_prob)
+        old_diff = sum(np.abs(np.array([irm.p(0) for _ in range(len(user_true_prob))]) - np.array(user_true_prob))) / len(user_true_prob)
+        new_diff = sum(np.abs(np.array(user_predict_prob) - np.array(user_true_prob))) / len(user_true_prob)
 
-    new_diff_abs = 0
-    old_diff_abs = 0
-    for i in range(len(user_true_prob)):
-        new_diff_abs += np.abs(user_predict_prob[i] - user_true_prob[i])
-        old_diff_abs += np.abs(user_predict_prob_origin[i] - user_true_prob[i])
-    new_diff_abs /= len(user_true_prob)
-    old_diff_abs /= len(user_true_prob)
+        res_dic[irm.model] = {}
+        res_dic[irm.model]["diff_new"] = new_diff
+        res_dic[irm.model]["diff_old"] = old_diff
+        res_dic[irm.model]["diff_acc"] = acc_diff
+        res_dic[irm.model]["b"] = irm.b
+        res_dic[irm.model]["a"] = irm.a
+        res_dic[irm.model]["origin_b"] = origin_b
+        res_dic[irm.model]["user_true_prob"] = np.mean(user_true_prob)
+        res_dic[irm.model]["user_old_prob"] = np.mean(user_old_prob)
+        res_dic[irm.model]["train_test_ratio"] = np.mean(train_test_ratios)
+        res_dic[irm.model]["user2theta"] = user2theta
 
-    return new_diff_abs, old_diff_abs
+    return res_dic
+
 
 ################################################################################################
-
-file = "/Users/zhangjieru/Documents/conan/irt/data/scores-04-"
-prepared_file = "/Users/zhangjieru/Documents/conan/irt/data/prepared_data"
+bath_path = os.path.abspath('..')
+file = bath_path + "/data/scores-04-"
+prepared_file = bath_path + "/data/prepared_data_0401_0419_10:5_3_3"
+save_path = bath_path + "/data/params"
 file_list_train = []
 file_list_test = []
 for item in ['01', '02', '03', '04', '05', '08', '09', '10', '11', '12']:
@@ -173,8 +225,8 @@ for item in ['01', '02', '03', '04', '05', '08', '09', '10', '11', '12']:
 for item in ['15', "16", '17', "18", '19']:
     file_list_test.append(file + item)
 
-
 if __name__ == '__main__':
+    # 加载处理好的数据，或者创建该数据
     if os.path.exists(prepared_file):
         with open(prepared_file, 'r') as f:
             shared_type2keys2users2score = eval(f.read())
@@ -185,22 +237,40 @@ if __name__ == '__main__':
         with open(prepared_file, 'w') as f:
             f.write(str(shared_type2keys2users2score))
 
-    diff_abs_old_total = []
-    diff_abs_new_total = []
-    # 获取训练集，测试集
-    start_total = time.time()
+    #开始训练每个知识点
+    res = {}
+    avg_res = {}
+    params = {}
     for skillType in list(shared_type2keys2users2score.keys())[1:]:
-        for key in list(shared_type2keys2users2score[skillType].keys())[:5]:
-            print("start training for keyword:" + skillType + '_' + key)
-            user2scores_train, user2scores_test, user2theta, total_ys_train, total_ys_test = getPreparedDataForOneKey(
-                shared_type2keys2users2score[skillType][key])
-            new_diff_abs, old_diff_abs = train(user2scores_train, user2scores_test, user2theta, total_ys_train, total_ys_test, iters=300, tolerance=1e-5)
-            print("new_diff_abs: ", new_diff_abs)
-            print("old_diff_abs: ", old_diff_abs)
-            diff_abs_new_total.append(new_diff_abs)
-            diff_abs_old_total.append(old_diff_abs)
-    end_total = time.time()
-    print("Total time consumption: ", end_total-start_total)
-    print("diff_abs_new_total: ", diff_abs_new_total)
-    print("diff_abs_old_total: ", diff_abs_old_total)
+        for key in list(shared_type2keys2users2score[skillType].keys())[:25]:
+            user_size = len(shared_type2keys2users2score[skillType][key].keys())
+            if user_size >= 400:
+                print("start training for keyword:" + skillType + '_' + key)
+                # 处理数据
+                user2scores_train, user2scores_test, user2theta, total_ys_train, total_ys_test = getPreparedDataForOneKey(shared_type2keys2users2score[skillType][key])
+                model_names = ["b", "ab", "bc", "abc"]
+                #开始训练
+                res_dic = train(user2scores_train, user2scores_test, user2theta, total_ys_train, total_ys_test, iters=500, tolerance=1e-2, model_names=model_names)
+                params[skillType + '_' + key] = res_dic
 
+                for model in ["b", 'ab', 'bc', 'abc']:
+                    if model in res.keys():
+                        res[model]["diff_acc"].append(res_dic[model]["diff_acc"])
+                        res[model]["diff_new"].append(res_dic[model]["diff_new"])
+                        res[model]["diff_old"].append(res_dic[model]["diff_old"])
+                    else:
+                        res[model] = {}
+                        res[model]["diff_acc"] = [res_dic[model]["diff_acc"]]
+                        res[model]["diff_new"] = [res_dic[model]["diff_new"]]
+                        res[model]["diff_old"] = [res_dic[model]["diff_old"]]
+    for model in ["b", 'ab', 'bc', 'abc']:
+        avg_res[model] = [np.mean(res[model]["diff_acc"]), np.mean(res[model]["diff_new"]),
+                          np.mean(res[model]["diff_old"])]
+
+    # 保存训练参数
+    with open(save_path, 'w') as f:
+        f.write(str(params) + "\n")
+        f.write(str(res) + "\n")
+        f.write(str(avg_res))
+
+    print(avg_res)
